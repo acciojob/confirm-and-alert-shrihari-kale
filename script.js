2f7eb8e3a979a1ed8fb2d09abb4b63ab9d33b7e1
Shrihari Kale <163184413+shrihari-kale@users.noreply.github.com>
2026-01-06 20:56:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const result = confirm("Do you want to proceed?");
+
+if (result) {
+	alert("You clicked OK. Proceeding...");
+}else {
+	alert("You clicked Cancel. Exiting...");
+}
+
+
+});
